0909510237b45297f2d8a35d8b0b6e6fbff9f755
imadcollin <imad.collin@gmail.com>
2020-12-21 20:49:22 +0100

    EspecialTokens

---
+++ codewar/src/main/java/codewar/iterators/EspecialStr.java
@@ -0,0 +1,24 @@
+package codewar.iterators;
+
+import javax.swing.text.html.HTMLDocument;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class EspecialStr {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("42");
+        arrayList.add("###");
+        arrayList.add("Hello");
+        arrayList.add("Java");
+        arrayList.add("4");
+        Iterator iterable = arrayList.iterator();
+        while (iterable.hasNext()) {
+            if (iterable.next().toString() == "###") {
+                while (iterable.hasNext()) {
+                    System.out.println(iterable.next().toString());
+                }
+            }
+        }
+    }
+}
