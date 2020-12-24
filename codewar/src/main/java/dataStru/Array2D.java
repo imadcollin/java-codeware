52cbd679954d3d0ca1798af0e3f679fc16f9dda4
imadcollin <imad.collin@gmail.com>
2020-12-24 23:05:29 +0100

    Fix ArrayD

--- codewar/src/main/java/dataStru/Array2D.java
+++ codewar/src/main/java/dataStru/Array2D.java
@@ -13,7 +13,7 @@ public class Array2D {
                 {0, 0, 0, 2, 0, 0},
                 {0, 0, 1, 2, 4, 0}
         };
-*/
+
         int  arr[][]= {
                 {1, 1, 1, 0, 0, 0},
                 {0, 1, 0, 0, 0, 0},
@@ -21,25 +21,37 @@ public class Array2D {
                 { 0, 9, 2, -4, -4, 0},
                 { 0, 0, 0, -2, 0, 0},
                 {0, 0, -1, -2, -4, 0}};
-
+                                         */
+        int arr [][]={
+                {-1, -1, 0, -9, -2, -2},
+                { -2, -1, -6, -8, -2, -5},
+                { -1, -1, -1, -2, -3, -4},
+                { -1 ,-9 ,-2 ,-4 ,-4, -5},
+                { -7 ,-3, -3, -2, -9, -9},
+                {-1 ,-3 ,-1 ,-2 ,-4 ,-5}}
+                ;
         int a=0, b=0;
         int sum = 0;
-        int large= 0 ;
+        int large=Integer.MIN_VALUE ;
         for(int x= 0 ; x<4;x++) {
             for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 3; j++) {
 
-                    a += arr[x][i + j];
-                    b += arr[x+2][i + j];
+                    a += (arr[x][i + j]);
+                    b += (arr[x+2][i + j]);
+
 
                 }
-                sum = a + b + arr[x+1][i + 1];
+
+                sum = (a) + (b) + (arr[x+1][i + 1]);
                 a = 0;
                 b = 0;
+
                 if (sum > large) large = sum;
             }
         }
         System.out.println(large);
+
 
     }
 
