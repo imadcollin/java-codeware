2a4a6210aa79ef2a53a73c603d8089ea12e822a1
imadcollin <imad.collin@gmail.com>
2020-12-28 23:26:03 +0100

    Equality Check

--- codewar/src/main/java/codewar/section4/Statment.java
+++ codewar/src/main/java/codewar/section4/Statment.java
@@ -147,5 +147,18 @@ class Statement {
         System.out.println(isEqual);
     }
 
+    public static boolean hasTeen(int a, int b, int c){
+        if((a >=13 && a<=19) || (b>=13 &&b <=19) ||(c >=13 && c <=19)) return true;
+        return false;
+    }
+    public static boolean isTeen(int a){
+        if(a>=13 && a <= 19) return true;
+        return false;
+    }
+    public static boolean hasEqualSum(int a, int b, int c){
+        if ((a+b) ==c) return true;
+        return false;
+    }
+
 }
 
