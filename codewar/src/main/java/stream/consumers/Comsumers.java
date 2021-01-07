9a045c96dc554d135822564c1566ff0aaf36bc5e
imadcollin <imad.collin@gmail.com>
2021-01-07 23:50:50 +0100

    Default exp1

--- codewar/src/main/java/stream/consumers/Comsumers.java
+++ codewar/src/main/java/stream/consumers/Comsumers.java
@@ -1,10 +1,22 @@
 package stream.consumers;
 
+import java.util.Locale;
 import java.util.function.Consumer;
 
 public class Comsumers {
     public static void main(String[]  args){
         Consumer<String> consumer= (s)->System.out.println(s);
         consumer.accept("hello");
+
+        firstAndThen("test");
+    }
+
+    public static void firstAndThen(String input){
+        Consumer<String> str1= (s1)->System.out.println(s1.toLowerCase());
+        Consumer<String> str2= (s2)->System.out.println(s2.toUpperCase());
+
+        Consumer<String> res=  str1.andThen(str2);
+        res.accept(input);
+
     }
 }
