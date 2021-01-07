197083d455e1b247b29f05c187e0947fd969c3cc
imadcollin <imad.collin@gmail.com>
2021-01-07 23:40:11 +0100

    Generate

--- codewar/src/main/java/stream/supliers/Suppliers.java
+++ codewar/src/main/java/stream/supliers/Suppliers.java
@@ -1,10 +1,18 @@
 package stream.supliers;
 
+import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Suppliers {
     public static void main (String[]  args){
         Supplier<String> supplier= ()->"test";
         System.out.println(supplier.get());
+
+        generateRandoms();
+    }
+    public static void generateRandoms(){
+        Supplier<Integer> numbers= ()-> new Random().nextInt(10);
+        Stream.generate(numbers).limit(5).forEach(System.out::println);
     }
 }
