101222605aa2a4629e48c9295dd7589f28792a48
imadcollin <imad.collin@gmail.com>
2021-01-07 23:58:33 +0100

    Student consumer

---
+++ codewar/src/main/java/stream/consumers/Main.java
@@ -0,0 +1,23 @@
+package stream.consumers;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(1, 12, "bob", "male");
+        Student s2 = new Student(2, 22, "moa", "female");
+        Student s3 = new Student(3, 18, "kent", "male");
+
+        List<Student> students = new ArrayList<>();
+        students.addAll(Arrays.asList(s1, s2, s3));
+
+        System.out.println(students.size());
+
+        Consumer<Student> studentConsumer= (s)->System.out.println(s);
+        students.forEach(studentConsumer);
+    }
+
+
+}
---
+++ codewar/src/main/java/stream/consumers/Student.java
@@ -0,0 +1,58 @@
+package stream.consumers;
+
+public class Student {
+    private int id;
+    private int age;
+    private String name;
+    private String gender;
+
+    public Student(int id, int age, String name, String gender) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
+
