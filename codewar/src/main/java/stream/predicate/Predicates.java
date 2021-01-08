06fa983baafa524b2cc2e21bfd47bbd2eeaed5ef
imadcollin <imad.collin@gmail.com>
2021-01-08 22:50:12 +0100

    Predicates both

--- codewar/src/main/java/stream/predicate/Predicates.java
+++ codewar/src/main/java/stream/predicate/Predicates.java
@@ -11,9 +11,17 @@ public class Predicates {
 
         boolean testing = isSmaller(i->i>10,20);
         System.out.println(testing);
+
+        boolean both = isGreaterAndSmaller(i->i>10,integer -> integer<20,15);
+        System.out.println(both);
     }
 
     public static boolean isSmaller(Predicate<Integer> predicate, int num){
         return predicate.test(num);
+    }
+
+    public static boolean isGreaterAndSmaller(Predicate<Integer> predicatem, Predicate<Integer> predicate2,int num1){
+        Predicate<Integer> both = predicatem.and(predicate2);
+      return   both.test(num1);
     }
 }
