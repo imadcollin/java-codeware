713d9338cd5b551448a80c9874f9261d673a8f14
imadcollin <imad.collin@gmail.com>
2021-01-14 22:50:20 +0100

    Add second sort

--- codewar/src/main/java/questions/SortingbyName.java
+++ codewar/src/main/java/questions/SortingbyName.java
@@ -6,46 +6,68 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class SortingbyName {
     public static void main(String args[]) {
-    
-        List<Employee> employees= new ArrayList<>(); 
-        Employee emp1= new Employee("John",26);
-        Employee emp2= new Employee("Martin",23);
-        Employee emp3= new Employee("John",20);
-        Employee emp4= new Employee("Martin",19);
-        Employee emp5= new Employee("Arpit",27);
-       
+
+        List<Employee> employees = new ArrayList<>();
+        Employee emp1 = new Employee("John", 26);
+        Employee emp2 = new Employee("Martin", 23);
+        Employee emp3 = new Employee("John", 20);
+        Employee emp4 = new Employee("Martin", 19);
+        Employee emp5 = new Employee("Arpit", 27);
+
         employees.add(emp1);
         employees.add(emp2);
         employees.add(emp3);
         employees.add(emp4);
         employees.add(emp5);
         System.out.println("List before sorting : ");
-        employees= Employee.sorting(employees); 
-        for(Employee e: employees)
-        {
-         System.out.println(e.name+" - "+e.age);
+        employees = Employee.sorting(employees);
+        for (Employee e : employees) {
+            System.out.println(e.name + " - " + e.age);
         }
-}
+
+        // Sort by names
+        employees = Employee.sorting(employees);
+        for (Employee e : employees) {
+            System.out.println(e.name + " - " + e.age);
+        }
+
+        // Sort by age
+        employees = Employee.sorting2(employees);
+
+        for (Employee e : employees) {
+            System.out.println(e.name + " - " + e.age);
+        }
+    }
 }
 
 class Employee {
     public String name;
     public int age;
-    
+
     public Employee(String name, int age) {
-     super();
-     this.name = name;
-     this.age = age;
+        super();
+        this.name = name;
+        this.age = age;
     }
-    public String getName(){
-        return name; 
+
+    public String getName() {
+        return name;
     }
-    public static List<Employee> sorting(List<Employee> list){
- Collections.sort(list, Comparator.comparing(Employee::getName));
- return list;
+
+    public int getAge() {
+        return age;
+    }
+
+    public static List<Employee> sorting(List<Employee> list) {
+        Collections.sort(list, Comparator.comparing(Employee::getName));
+        return list;
+    }
+
+    public static List<Employee> sorting2(List<Employee> list) {
+        return list.stream().sorted(Comparator.comparing(Employee::getAge)).collect(Collectors.toList());
     }
 }
-
