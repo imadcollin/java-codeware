720f644529ec863d3658b22b50412af37c9d5436
imadcollin <imad.collin@gmail.com>
2021-01-19 23:58:28 +0100

    Add sorting

--- codewar/src/main/java/codewar/company/Department.java
+++ codewar/src/main/java/codewar/company/Department.java
@@ -1,6 +1,7 @@
 package codewar.company;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,6 +24,9 @@ public class Department {
 
     public List<Employee> removeEmployee(String name) {
         return employees.stream().filter(x -> (!  name.equals(x.getName()))).collect(Collectors.toList());
+    }
+    public static List<Employee> sortEmp(List<Employee> list){
+        return list.stream().sorted(Comparator.comparing(list,Employee::getName)).collect(Collectors.toList());
     }
 
 }
