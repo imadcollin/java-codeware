44e031c9945b8e1b7e6a83040aa85dd60a3eef8e
imadcollin <imad.collin@gmail.com>
2021-01-19 21:12:41 +0100

    Sort names and  ages

--- codewar/src/main/java/codewar/arraylist/Sorter.java
+++ codewar/src/main/java/codewar/arraylist/Sorter.java
@@ -8,20 +8,26 @@ import java.util.stream.Collectors;
 
 public class Sorter {
     public static void main(String[] args) {
-        Employee e1 = new Employee(22, "bob", new Date(1999, 3, 12));
         Employee e2 = new Employee(33, "moa", new Date(2020, 3, 12));
+        Employee e1 = new Employee(22, "bob", new Date(1999, 3, 12));
         Employee e3 = new Employee(44, "adam", new Date(2020, 3, 12));
+        Employee e4 = new Employee(11, "eva", new Date(2020, 3, 12));
 
         List<Employee> list = new ArrayList<>();
         list.add(e1);
         list.add(e2);
         list.add(e3);
+        list.add(e4);
 
         Employee.printList(list);
         System.out.println("---------------------");
         list = Employee.nameSort(list);
         Employee.printList(list);
 
+        list.sort(new AgeSorting());
+
+        System.out.println("---------------------");
+        Employee.printList(list);
     }
 
 }
@@ -39,7 +45,11 @@ class Employee {
     }
 
     public String getName() {
-        return this.name;
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     @Override
@@ -54,4 +64,13 @@ class Employee {
     public static void printList(List<Employee> list) {
         list.stream().forEach(System.out::println);
     }
+}
+
+class AgeSorting implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getAge() > o2.getAge() ? 1 : o1.getAge() < o2.getAge() ? -1 : 0;
+    }
+
 }
